7db9ee5a9b1fc1ad7741f7028ffe7359087a627a
JoseIgnacioDayer <nachodayer@gmail.com>
2025-11-02 20:17:31 -0300

    ci: agregar workflow de GitHub Actions con Jest y Cucumber

---
+++ features/step_definitions/stepdefs.js
@@ -0,0 +1,18 @@
+import assert from 'assert';
+import { Given, When, Then } from '@cucumber/cucumber';
+import { Ahorcado } from '../../src/resources/ahorcado';
+let ahorcado;
+const letras = ['M', 'A', 'N', 'Z'];
+Given('La palabra MANZANA', function () {
+    console.log('Given: La palabra MANZANA');
+    ahorcado = new Ahorcado('manzana');
+});
+When('Adivino todas las letras', function () {
+    console.log('When: Adivino todas las letras');
+    letras.forEach((letra) => ahorcado.adivinar_letra(letra));
+});
+Then('Deberia mostrarme que gane', function () {
+    console.log('Then: Deberia mostrarme que gane');
+    console.log('Estado del juego:', ahorcado.es_victoria_o_es_derrota());
+    assert.strictEqual(ahorcado.es_victoria_o_es_derrota(), 'victoria');
+});
