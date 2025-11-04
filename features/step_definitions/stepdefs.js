c2cd41f92b65eb2c5427ff1e6a28cade148e484d
BorsatoMilton <borsatomilton@gmail.com>
2025-11-03 23:39:03 -0300

    Mejora de sintaxis en features de cucumber

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,64 +1,77 @@
 import assert from 'assert';
 import { Given, When, Then } from '@cucumber/cucumber';
-import { Ahorcado } from '../../src/resources/ahorcado';
+import { Ahorcado } from '../../src/resources/models/ahorcado';
 let ahorcado;
-const letras = ['M', 'A', 'N', 'Z'];
-Given('La palabra MANZANA', function () {
-    console.log('Given: La palabra MANZANA');
-    ahorcado = new Ahorcado('manzana');
+let palabra;
+Given(/una palabra cualquiera/i, function () {
+  console.log('Given: Una palabra cualquiera');
+  ahorcado = new Ahorcado(undefined, 'spanish', 'easy');
 });
-When('Adivino todas las letras', function () {
-    console.log('When: Adivino todas las letras');
-    letras.forEach((letra) => ahorcado.adivinar_letra(letra));
+When(/adivino todas las letras/i, function () {
+  console.log('When: Adivino todas las letras');
+  palabra = ahorcado.informar_palabra_secreta();
+  palabra.split('').forEach((letra) => ahorcado.adivinar_letra(letra));
 });
-Then('Deberia mostrarme que gane', function () {
-    console.log('Then: Deberia mostrarme que gane');
-    console.log('Estado del juego:', ahorcado.es_victoria_o_es_derrota());
-    assert.strictEqual(ahorcado.es_victoria_o_es_derrota(), 'victoria');
+When(/intento con letras incorrectas todo el juego/i, function () {
+  console.log('When: Intento con letras incorrectas todo el juego');
+  palabra = ahorcado.informar_palabra_secreta();
+  const letras_no_pertenecientes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    .split('')
+    .filter((letra) => !palabra.includes(letra));
+  for (const letra of letras_no_pertenecientes) {
+    if (ahorcado.informar_intentos_restantes() <= 0) break;
+    ahorcado.adivinar_letra(letra);
+  }
 });
-
-// ------------------------------------------- Escenario el_peor_juego.feature: Derrota sin aciertos -------------------------------------------
-Given('La palabra MANGO', function () {
-    console.log('Given: La palabra MANGO');
-    ahorcado = new Ahorcado('mango');
+When(/intento con letras incorrectas y luego completo la palabra/i, function () {
+  console.log('When: intento con letras incorrectas y luego completo la palabra');
+  palabra = ahorcado.informar_palabra_secreta();
+  const letras_no_pertenecientes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    .split('')
+    .filter((letra) => !palabra.includes(letra))
+    .slice(0, 2)
+    .join('');
+  letras_no_pertenecientes.split('').forEach((letra) => ahorcado.adivinar_letra(letra));
+  palabra.split('').forEach((letra) => ahorcado.adivinar_letra(letra));
 });
-When('Intento con letras incorrectas todo el juego', function () {
-    console.log('When: Intento con letras incorrectas todo el juego');
-    const letras_incorrectas = ['X', 'Y', 'Z', 'W', 'V', 'U'];
-    letras_incorrectas.forEach((letra) => ahorcado.adivinar_letra(letra));
+When(/adivino algunas letras correctas y intento letras incorrectas/i, function () {
+  console.log('When: Adivino algunas letras correctas y intento letras incorrectas');
+  palabra = ahorcado.informar_palabra_secreta();
+  const letras_correctas = palabra.split('').slice(0, 2).join('');
+  const letras_no_pertenecientes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    .split('')
+    .filter((letra) => !palabra.includes(letra));
+  for (const letra of letras_correctas.split('')) {
+    ahorcado.adivinar_letra(letra);
+  }
+  for (const letra of letras_no_pertenecientes) {
+    if (ahorcado.informar_intentos_restantes() <= 0) break;
+    ahorcado.adivinar_letra(letra);
+  }
 });
-Then('Deberia mostrarme que perdi', function () {
-    console.log('Then: Deberia mostrarme que perdi');
-    console.log('Estado del juego:', ahorcado.es_victoria_o_es_derrota());
-    assert.strictEqual(ahorcado.es_victoria_o_es_derrota(), 'derrota');
+When(/supero el limite de intentos/i, function () {
+  console.log('When: Supero el limite de intentos');
+  palabra = ahorcado.informar_palabra_secreta();
+  const letras_no_pertenecientes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    .split('')
+    .filter((letra) => !palabra.includes(letra));
+  for (const letra of letras_no_pertenecientes) {
+    if (ahorcado.informar_intentos_restantes() <= 0) break;
+    ahorcado.adivinar_letra(letra);
+  }
 });
-// ------------------------------------------- Escenario gano_con_algunos_errores.feature: victoria con algunos errores -------------------------------------------
-Given('La palabra PERA', function () {
-    console.log('Given: La palabra PERA');
-    ahorcado = new Ahorcado('pera');
+Then(/deberia mostrarme que gane/i, function () {
+  console.log('Then: Deberia mostrarme que gane');
+  console.log('Estado del juego:', ahorcado.es_victoria_o_es_derrota());
+  assert.strictEqual(ahorcado.es_victoria_o_es_derrota(), 'victoria');
 });
-When('Adivino algunas letras correctas y otras incorrectas', function () {
-    console.log('When: Adivino algunas letras correctas y otras incorrectas');
-    const letras_a_adivinar = ['P', 'E', 'X', 'Y', 'Z'];
-    letras_a_adivinar.forEach((letra) => ahorcado.adivinar_letra(letra));
+Then(/deberia mostrarme que perdi/i, function () {
+  console.log('Then: Deberia mostrarme que perdi');
+  console.log('Estado del juego:', ahorcado.es_victoria_o_es_derrota());
+  assert.strictEqual(ahorcado.es_victoria_o_es_derrota(), 'derrota');
 });
-Then('Deberia mostrarme que gane', function () {
-    console.log('Then: Deberia mostrarme que gane');
-    console.log('Estado del juego:', ahorcado.es_victoria_o_es_derrota());
-    assert.strictEqual(ahorcado.es_victoria_o_es_derrota(), 'victoria');
-});
-// ------------------------------------------- Escenario pierdo_con_algunos_aciertos.feature: Derrota con algunos aciertos -------------------------------------------
-Given('La palabra MANGO', function () {
-    console.log('Given: La palabra MANGO');
-    ahorcado = new Ahorcado('mango');
-});
-When('Adivino algunas letras correctas y intento letras incorrectas', function () {
-    console.log('When: Adivino algunas letras correctas y intento letras incorrectas');
-    const letras_a_adivinar = ['M', 'A', 'X', 'Y', 'Z', 'W', 'V', 'U'];
-    letras_a_adivinar.forEach((letra) => ahorcado.adivinar_letra(letra));
-});
-Then('Deberia mostrarme que perdi', function () {
-    console.log('Then: Deberia mostrarme que perdi');
-    console.log('Estado del juego:', ahorcado.es_victoria_o_es_derrota());
-    assert.strictEqual(ahorcado.es_victoria_o_es_derrota(), 'derrota');
+Then(/deberia mostrarme los intentos restantes/i, function () {
+  console.log('Then: Deberia mostrarme los intentos restantes');
+  console.log('Intentos restantes:', ahorcado.informar_intentos_restantes());
+  assert.strictEqual(typeof ahorcado.informar_intentos_restantes(), 'number');
 });
--- features/step_definitions/stepdefs.ts
+++ features/step_definitions/stepdefs.ts
@@ -5,82 +5,85 @@ import { Ahorcado } from '../../src/resources/models/ahorcado';
 let ahorcado: Ahorcado;
 let palabra: string;
 
-//-------------------------------------- Escenario juego_perfecto.feature: victoria sin errores ----------------------------------------------------
-Given('Una palabra cualquiera', function () {
+Given(/una palabra cualquiera/i, function () {
   console.log('Given: Una palabra cualquiera');
   ahorcado = new Ahorcado(undefined, 'spanish', 'easy');
 });
 
-When('Adivino todas las letras', function () {
+When(/adivino todas las letras/i, function () {
   console.log('When: Adivino todas las letras');
   palabra = ahorcado.informar_palabra_secreta();
   palabra.split('').forEach((letra) => ahorcado.adivinar_letra(letra));
 });
 
-Then('Deberia mostrarme que gane', function () {
-  console.log('Then: Deberia mostrarme que gane');
-  console.log('Estado del juego:', ahorcado.es_victoria_o_es_derrota());
-  assert.strictEqual(ahorcado.es_victoria_o_es_derrota(), 'victoria');
-});
-
-// ------------------------------------------- Escenario el_peor_juego.feature: Derrota sin aciertos -------------------------------------------
-Given('Una palabra cualquiera', function () {
-  console.log('Given: Una palabra cualquiera');
-  ahorcado = new Ahorcado(undefined, 'spanish', 'easy');
-});
-
-When('Intento con letras incorrectas todo el juego', function () {
+When(/intento con letras incorrectas todo el juego/i, function () {
   console.log('When: Intento con letras incorrectas todo el juego');
-  palabra = ahorcado.informar_palabra_secreta()
-  const letras_no_pertenecientes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').filter((letra) => !palabra.includes(letra)).slice(0, 6).join('');
-  letras_no_pertenecientes.split('').forEach((letra) => ahorcado.adivinar_letra(letra));
+  palabra = ahorcado.informar_palabra_secreta();
+  const letras_no_pertenecientes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    .split('')
+    .filter((letra) => !palabra.includes(letra));
+
+  for (const letra of letras_no_pertenecientes) {
+    if (ahorcado.informar_intentos_restantes() <= 0) break;
+    ahorcado.adivinar_letra(letra);
+  }
 });
 
-Then('Deberia mostrarme que perdi', function () {
-  console.log('Then: Deberia mostrarme que perdi');
-  console.log('Estado del juego:', ahorcado.es_victoria_o_es_derrota());
-  assert.strictEqual(ahorcado.es_victoria_o_es_derrota(), 'derrota');
-});
-
-// ------------------------------------------- Escenario gano_con_algunos_errores.feature: victoria con algunos errores -------------------------------------------
-Given('Una palabra cualquiera', function () {
-  console.log('Given: Una palabra cualquiera');
-  ahorcado = new Ahorcado(undefined, 'spanish', 'easy');
-});
-When('intento con letras incorrectas y luego completo la palabra', function () {
+When(/intento con letras incorrectas y luego completo la palabra/i, function () {
   console.log('When: intento con letras incorrectas y luego completo la palabra');
   palabra = ahorcado.informar_palabra_secreta();
-  const letras_no_pertenecientes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').filter((letra) => !palabra.includes(letra)).slice(0, 2).join(''); // Tomo solo 2 letras incorrectas, luego completo la palabra
+  const letras_no_pertenecientes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    .split('')
+    .filter((letra) => !palabra.includes(letra))
+    .slice(0, 2)
+    .join('');
   letras_no_pertenecientes.split('').forEach((letra) => ahorcado.adivinar_letra(letra));
   palabra.split('').forEach((letra) => ahorcado.adivinar_letra(letra));
 });
-// Then Deberia mostrarme que gane And Deberia mostrarme los intentos restantes
-Then('Deberia mostrarme que gane', function () {
+
+When(/adivino algunas letras correctas y intento letras incorrectas/i, function () {
+  console.log('When: Adivino algunas letras correctas y intento letras incorrectas');
+  palabra = ahorcado.informar_palabra_secreta();
+  const letras_correctas = palabra.split('').slice(0, 2).join('');
+  const letras_no_pertenecientes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    .split('')
+    .filter((letra) => !palabra.includes(letra));
+
+  for (const letra of letras_correctas.split('')) {
+    ahorcado.adivinar_letra(letra);
+  }
+  for (const letra of letras_no_pertenecientes) {
+    if (ahorcado.informar_intentos_restantes() <= 0) break;
+    ahorcado.adivinar_letra(letra);
+  }
+});
+
+When(/supero el limite de intentos/i, function () {
+  console.log('When: Supero el limite de intentos');
+  palabra = ahorcado.informar_palabra_secreta();
+  const letras_no_pertenecientes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    .split('')
+    .filter((letra) => !palabra.includes(letra));
+  for (const letra of letras_no_pertenecientes) {
+    if (ahorcado.informar_intentos_restantes() <= 0) break;
+    ahorcado.adivinar_letra(letra);
+  }
+});
+
+Then(/deberia mostrarme que gane/i, function () {
   console.log('Then: Deberia mostrarme que gane');
   console.log('Estado del juego:', ahorcado.es_victoria_o_es_derrota());
   assert.strictEqual(ahorcado.es_victoria_o_es_derrota(), 'victoria');
 });
-Then('Deberia mostrarme los intentos restantes', function () {
-  console.log('Then: Deberia mostrarme los intentos restantes');
-  console.log('Intentos restantes:', ahorcado.informar_intentos_restantes());
-  assert.strictEqual(ahorcado.informar_intentos_restantes(), 0);
-});
 
-// --------------- Escenario pierdo_con_algunos_aciertos.feature: derrota con algunos aciertos -------------------------------------------
-Given('Una palabra cualquiera', function () {
-  console.log('Given: Una palabra cualquiera');
-  ahorcado = new Ahorcado(undefined, 'spanish', 'easy');
-});
-When('Adivino algunas letras correctas y intento letras incorrectas', function () {
-  console.log('When: Adivino algunas letras correctas y intento letras incorrectas');
-  palabra = ahorcado.informar_palabra_secreta();
-  const letras_correctas = palabra.split('').slice(0, 2).join(''); // Tomo solo 2 letras correctas
-  const letras_no_pertenecientes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').filter((letra) => !palabra.includes(letra)).slice(0, 6).join(''); // Tomo 6 letras incorrectas
-  letras_correctas.split('').forEach((letra) => ahorcado.adivinar_letra(letra));
-  letras_no_pertenecientes.split('').forEach((letra) => ahorcado.adivinar_letra(letra));
-});
-Then('Deberia mostrarme que perdi', function () {
+Then(/deberia mostrarme que perdi/i, function () {
   console.log('Then: Deberia mostrarme que perdi');
   console.log('Estado del juego:', ahorcado.es_victoria_o_es_derrota());
   assert.strictEqual(ahorcado.es_victoria_o_es_derrota(), 'derrota');
+});
+
+Then(/deberia mostrarme los intentos restantes/i, function () {
+  console.log('Then: Deberia mostrarme los intentos restantes');
+  console.log('Intentos restantes:', ahorcado.informar_intentos_restantes());
+  assert.strictEqual(typeof ahorcado.informar_intentos_restantes(), 'number');
 });
